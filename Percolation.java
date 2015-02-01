0a38c95202f3e7b4dc951c04924453168e0d08b5
Emily Chen <ec2805@columbia.ed>
2015-01-31 20:12:36 -0500

    stubbed interface

---
+++ Percolation.java
@@ -0,0 +1,26 @@
+public class Percolation    {
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N)   {
+        TODO
+    }
+    
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j)  {
+        TODO
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        TODO
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        TODO
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        TODO
+    }
+}
---
+++ PercolationStats.java
@@ -0,0 +1,33 @@
+public class PercolationStats   {
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T)   {
+        TODO
+    }
+
+    // sample mean of percolation threshold
+    public double mean()    {
+        TODO
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev()  {
+        TODO
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo()    {
+        TODO
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh()  {
+        TODO
+    }
+
+    // takes two command line args N, T
+    // performs T independent experiments on an N-by-N grid
+    // prints out the mean, std dev, and 95% confidence interval for the percolation threshold
+    public static void main(String[] args)  {
+        TODO
+    }
+}
